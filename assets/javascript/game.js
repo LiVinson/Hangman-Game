f5a173ad7a000738f9b59aae9a19151dc33e51eb
Lisa Vinson <Lrv0828@gmail.com>
2017-11-07 18:15:47 -0500

    JS updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,86 @@
 //DECLARE VARIABLES
 
 //Words that user will have to guess, randomly selected
-var wordBank = ["Florida Gators",
-                "Georgia Bulldogs", 
-                "Tennessee Volunteers", 
-                "South Carolina Gamecocks",
-                "Vanderbilt Commodores",
-                "Kentucky Wildcats",
-                "Alabama Crimson Tide",
-                "Louisiana State Tigers",
-                "Ole Miss Rebels",
-                "Mississippi State Bulldogs",
-                "Texas A&M Aggies",
-                "Auburn Tigers",
-                "Arkansas Razorbacks",
-                "Missouri Tigers"];
+var wordBank = ["florida_gators",
+                "georgia_bulldogs", 
+                "tennessee_volunteers", 
+                "south_carolina_gamecocks",
+                "vanderbilt_commodores",
+                "kentucky_wildcats",
+                "alabama_crimson_tide",
+                "louisiana_state_tigers",
+                "ole_miss_rebels",
+                "mississippi_state_bulldogs",
+                "texas_am_aggies",
+                "auburn_tigers",
+                "arkansas_razorbacks",
+                "missouri_tigers"];
 
  //Creates an empty array to hold the users guesses; will need to reset at the end of each game
 var previousGuesses = [];
 
-
-                //Equals the number of guesses user has used; resets after each question or stops the game if reaches a certain number
+//Equals the number of guesses user has used; resets after each question or stops the game if reaches a certain number (0)
 var guessesRemaining = 10;
 
-//Selects the html start button. And adds an event listening: for the user to click on the button. When this occuts, a TBD function is called
-document.getElementById("startButton").addEventListener("click", startGame());
 
+document.onkeyup = function (event) {
 
-function startGame(){
-    //Have the entire intro continer disappear, game container appears. (POssible to toggle?)
-    
-     // = A randomly chosen item from the wordBank array
-    var wordToGuess = Math.floor(Math.random()*[wordBank.length]);
+    //Makes value of key pressed lowercase and a string. Assigns value to userGuess    
+    var userGuess = (event.keycode)
 
+     // A randomly chosen item from the wordBank array
+    var wordToGuess = wordBank[Math.floor(Math.random() * wordBank.length)];
 
+        console.log(wordToGuess);
+    // Sets variable to the length of the word chosen.
+    var wordToGuessLength = wordToGuess.length;
+
+    var underline = "_"
+    var space = " "
+
+        console.log(wordToGuessLength);
     //Empty array
-    var numberUnderlines = []; //When a letter is guessed, it replaces the underline in the word
-    var wordDisplayed = String(numberUnderlines);//make this variable = numberUnderlines as a string, and then have a paragraph display it
+    var numberUnderlines = []; 
+
+        //For each index in wordToGuess: add "an underscore" or a blank into the numberUnderlines array
+    for (i = 0; i < wordToGuessLength; i++){
+        if(wordToGuess[i] !== "_")
+        numberUnderlines.push("underline");
+
+        else
+        numberUnderlines.push("A");      
+    } 
+    console.log(numberUnderlines)
+    //Once underline/spaces are each an item of underline array, turn it into one string
+    var wordDisplayed = numberUnderlines.join("");
+
+    //Set value (string) of the underline string to display in HTML
+    document.getElementById("wordDisplayed").innerHTML = numberUnderlines;
+}
+  
+
    
     
-    //Set the number of "underscores" on display equal to the number of characters in the randomly chosen item from the array
-    //For each index in wordToGuess: add "an underscore" 
-    for (i = 0; i < wordToGuess.length; i++) {
-        numberUnderlines.push("_");
-            
-    }
-    
+
+
     //Set an event to listen to the key pressed, and to set the value of the pressed key(first letter, turned into a string) = userGuess varible
 
     // Captures the key press, converts it to lowercase, and saves it to variable userGuess.
-    var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+    
+    
+    //  CODEvar userGuess = String.fromCharCode(event.key).toUpperCase();
 
         //Compares the userguess to all of letters previously guessed (array starts empty, but then userGuess is letters added)
-        if(userGuess === previousGuesses){/*Figure out how to compare userGuess to each item in previous guesses */
+        //CODE if(userGuess === previousGuesses){/*Figure out how to compare userGuess to each item in previous guesses */
             //Don't do anything (or maybe play some kind of boink sound)
 
 
-        }else(
+        //CODE }else(
             
-            previousGuesses.push(userGuess)
+            // CODE previousGuesses.push(userGuess)
             //Call some kind of function
-        );
+         // CODE);
     
 
     //Need to do a test to see if the userGuess (letter pressed) is equal 1)to any letter in the wordToGuess index AND that it has not lready been guessed (on the previous guess list)
   
-}
