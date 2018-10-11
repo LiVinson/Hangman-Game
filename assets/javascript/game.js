522a1cf4de0c4752831769c64b1bcaba6541b30d
Lisa Vinson <lisa.vinson@superion.com>
2018-10-11 14:13:35 -0400

    fixed restart logic, removed unneeded functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,4 @@
-//----DECLARE VARIABLES (Globally Defined) -------------------------------------------------------------------------------------------
-
-
-//Starting Logic:
-//chooseWordToGuess(): Choose a random word and split into array - store as wordArray
-//changeCharToUnderScore(): For each character in the word, swap a "_" for the underscore and store underscoreArray
-//createWordString: Change the udnerscore into a string with proper spaces
-//Update the text on screen with word values
-
-//On button press:
-
-//confirmGuessValid: Check if user pressed a valid key
-//compareGuessToWord: If wrong, decrements guessesLeft, checks if game is over;
-//If correct, decrements numLetterstoGuess, calls to createWordString for update
-
-
-//Each time user makes a correct guess, replace the index of the correct guess in underscoreArray with the value of
-//same index from wordArray (don't change it all just the match. Should keep anything changed previously) - will need to splice string
-
-//Call wordString again to update the string and resave working Guess.
-//Updates guesses remaining, etc
-
-//Redisplay string (with letters where correct.)
-
-
-//Words that user will have to guess, randomly selected
+//----GLOBAL VARIABLES-----------------------------------------------------------------------------------------
 
 const WORDBANK = [
     "FLORIDA GATORS",
@@ -70,26 +45,25 @@ let gameText = {
     lossesDisplay: document.getElementById("losses")
 }
 
-//Dynamically displays text on screen based on current game stats
+//------FUNCTION DECLARATIONS ---------------------------------------------------------------------
 function setScoreInfo() {
-
     if (gameStats.previousGuesses.length) {
         gameText.previouslyGuessedDisplay.innerHTML = gameStats.previousGuesses.join(", ")
     } else {
-        console.log("nothing guessed yet");
         gameText.previouslyGuessedDisplay.innerHTML = "Any letters you have guessed will display here."
     }
     gameText.guessesRemainingDisplay.innerHTML = gameStats.guessesRemaining;
     gameText.winsDisplay.innerHTML = gameStats.wins;
     gameText.lossesDisplay.innerHTML = gameStats.losses;
     gameText.gameMessageDisplay.innerHTML = gameStats.gameMessage;
-    gameText.wordToGuessDisplay.innerHTML =  gameStats.guessWordWorkingString;
+    gameText.wordToGuessDisplay.innerHTML = gameStats.guessWordWorkingString;
     return
 }
 
 function chooseWordToGuess() {
     gameStats.guessWordIndex = Math.floor(Math.random() * WORDBANK.length);
     gameStats.guessWordActual = WORDBANK[gameStats.guessWordIndex].split(""); //splits the string into an array including spaces/ampersands
+    changeCharToUnderScore()
 }
 
 //Input: Each letter of word to be guessed in an array;
@@ -98,8 +72,6 @@ function changeCharToUnderScore() {
     let guessWordArr = gameStats.guessWordActual;
     let blankWordArr = [];
     let numOfLetters = 0;
-    //for each letter in the displayWord, determine if it is a letter, a space, or an ampersand
-    console.log("chosen word in array before swapping for _", guessWordArr);
 
     guessWordArr.forEach(function (char, index) {
         if (ALPHABET_LIST.indexOf(char) > -1) { //checks if character is a letter or not, and if so replaces it with underscore
@@ -111,11 +83,17 @@ function changeCharToUnderScore() {
     });
     gameStats.guessWordCharsLeftToGuess = numOfLetters;
     gameStats.guessWordBlanks = blankWordArr; //This is an array with blanks for letters
+    createWordString();
+
 };
 
 function createWordString() {
+    let wordArr = gameStats.guessWordBlanks;
+    if (gameStats.gameOver) {
+        wordArr = gameStats.guessWordActual
+    }
     let wordString = "";
-    gameStats.guessWordBlanks.forEach(function (char) {
+    wordArr.forEach(function (char) {
         if (char === " ") {
             wordString += '&nbsp'; //If it's a space, add encoded space
         } else {
@@ -126,21 +104,11 @@ function createWordString() {
     gameStats.guessWordWorkingString = wordString;
 
     setScoreInfo();
-    return;
 }
-
-// console.log("word in array after swapping letters for underscores", displayWordArr)
-
-// console.log("after changing from array to string:", displayWordArr.join(" "));
-// gameStats.guessWordBlanks = displayWordArr.join(" ");
-// gameStats.guessWordCharsLeftToGuess = displayWordArr.length - numbOfNonLetters;
-// }
-
 
 function confirmGuessValid(event) {
     gameStats.gameMessage = "";
     userChoice = event.key.toUpperCase();
-    console.log("userChoice: ", userChoice);
     // Compares the value of userChoice (key pressed) to each item in the alphabet Array. If the key press is not a letter, then return an error message
     if (ALPHABET_LIST.indexOf(userChoice) === -1) {
         gameStats.message = "Invalid character. Please select a letter A-Z";
@@ -148,12 +116,10 @@ function confirmGuessValid(event) {
 
         //If key pressed is a letter then proceed to the next test
     } else if (gameStats.previousGuesses.indexOf(userChoice) > -1) {
-        console.log("This was  guessed previously, guess again!");
         gameStats.message = "The letter " + userChoice + " was already guessed. Please guess again.";
         return;
     } else {
-        console.log("This is a unique guess");
-        gameStats.previousGuesses.unshift(userChoice);
+        gameStats.previousGuesses.push(userChoice);
         //Call the compare function (defined below) that compares the userChoice to the characters in the random word
         compareGuessToWord(userChoice);
     }
@@ -173,11 +139,8 @@ function compareGuessToWord(userGuess) {
             //If the choice is a match at a partiular index position, update the corresponding index in the guessWordBlank array
             if (userGuess === gameStats.guessWordActual[k]) {
                 gameStats.guessWordBlanks[k] = userGuess;
-                console.log("After replacing the underlines with the user choice where there was a match: " + gameStats.guessWordBlanks)
-
                 //numberToGuess is set to number of characters (nonspaces); For each match the userguesses, the number reduces
                 gameStats.guessWordCharsLeftToGuess--;
-                console.log("Number or letters remaining to guess: ", gameStats.guessWordCharsLeftToGuess)
             }
         }
         //Once all of the matching letters have been displayed for the guess, updates the HTML so it show on the screen.       
@@ -185,28 +148,22 @@ function compareGuessToWord(userGuess) {
     checkGameOver();
 }
 
-//Needed?
-function displayFullWord(wordIndex) {
-    let displayWord = WORDBANK[wordIndex];
-
-
-}
 
 //  GAMEOVER FUNCTIONS
-
 function checkGameOver() {
 
     if (gameStats.guessWordCharsLeftToGuess < 1) { //user won
         gameStats.wins++;
         gameStats.gameMessage = "Congrats! You won! Press the spacebar key to play again."
         gameStats.gameOver = true;
-        setScoreInfo()
+        createWordString()
+
     } else if (gameStats.guessesRemaining === 0) { //user lost
-        gameStats.lossess++;
-        gameStats.gameMessage = `Game Over! The correct answer is ${WORDBANK[gameStats.wordIndex]}. Press  spacebar to play again.`
-        gameStats.guessWordWorkingString = WORDBANK[gameStats.wordIndex]
+        gameStats.losses++;
+        gameStats.gameMessage = `Game Over! The correct answer is ${WORDBANK[gameStats.guessWordIndex]}. Press  any key to play again.`
+        gameStats.guessWordBlanks = WORDBANK[gameStats.guessWordIndex]
         gameStats.gameOver = true;
-        setScoreInfo()
+        createWordString()
     } else {
         console.log("game's not over, keep guessing!")
         createWordString()
@@ -214,56 +171,33 @@ function checkGameOver() {
 }
 
 
-//Listens for user to press space bar to reset the game
-document.onkeyup = function (event) {
-    console.log(event.keycode)
-
-    if (event.keycode == 32) {
-
-        //Resets guesses
-        previousGuesses = [];
-        guessesRemaining = 6;
-
-        //Chooses new word, and updates length and display word previously defined to correspond to last word chosen
-        guessWord = WORDBANK[Math.floor(Math.random() * WORDBANK.length)];
-        wtgLength = guessWord.length;
-        wtgDisplayed = guessWord;
-
-        //Resets the word displayed on the screen
-        for (i = 0; i < wtgLength; i++) {
-
-            if (guessWord[i] !== " ") {
-                //If value is not a space, then replace that letter with an underscore
-                wtgDisplayed = wtgDisplayed.replace(guessWord[i], "_");
-            } else {
-                numberOfSpaces++ //For each character that is a space logs a count.
-            }
-        }
-
-        wtgDisplayedSpace = wtgDisplayed //wtgDisplayedSpace has underlines and a traditional space
-
-        wtgDisplayed = wtgDisplayed.replace(/spaceRegular/g, spaceHTML); //replaces the traditional space with the "&nbsp;" so it can display in HTML
-
-        //Determine how many letters user needs to guess to win
-        numberToGuess = wtgLength - numberOfSpaces;
-
-        //Sets html text of the underlines div to the value of wtgDisplayed (Which should now be underlines)
-        underlinesDisplayed.innerHTML = wtgDisplayed;
-    }
+function initiateGame() {
+    gameStats.previousGuesses = [],
+        gameStats.guessesRemaining = 6,
+        gameStats.guessWordIndex = null, //index of word to guess in wordbank array
+        gameStats.guessWordActual = null, //This is the word that was chosen from the array, as an array
+        gameStats.guessWordBlanks = null, //Word chosen with _ for words, as an array
+        gameStats.guessWordWorkingString = null, //This is what will be used to display, and will be updated after each guess. Will have space concatenated into string
+        gameStats.guessWordCharsLeftToGuess = null, //This is the number of chars to guess. Equals # of letters - spaces/amp in array
+        gameStats.gameMessage = "",
+        gameStats.gameOver = false
+    chooseWordToGuess();
 }
-
-
-chooseWordToGuess();
-changeCharToUnderScore();
-createWordString();
 
 
 //Listens for a key to be pressed. WHen this occurs, the following block of code is run each time:
 document.onkeyup = function (event) {
-    confirmGuessValid(event)
+    if (gameStats.gameOver) {
+        gameStats.gameOver = false;
+        initiateGame();
+    } else {
+        confirmGuessValid(event)
+    }
+
 
 };
 
+chooseWordToGuess();
 
 
 
